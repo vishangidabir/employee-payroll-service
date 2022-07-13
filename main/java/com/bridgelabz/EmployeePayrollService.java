74cf20f64a863cfb757bca2ecac3ee0b10de2d38
vishangi <vishangid@gmail.com>
2022-07-13 18:40:58 +0530

    Welcome message

---
+++ main/java/com/bridgelabz/EmployeePayrollService.java
@@ -0,0 +1,41 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmployeePayrollService {
+    private List<EmployeePayrollData> employeePayrollList;
+
+    public EmployeePayrollService() {
+
+    }
+
+    public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
+        this.employeePayrollList = employeePayrollList;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to employee payroll program");
+        ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
+        Scanner consoleInputReader = new Scanner(System.in);
+        employeePayrollService.readEmployeePayrollData(consoleInputReader);
+        employeePayrollService.writeEmployeePayrollData();
+    }
+
+    public void writeEmployeePayrollData() {
+        System.out.println("Writing employee payroll roaster to console " + employeePayrollList);
+    }
+
+    private void readEmployeePayrollData(Scanner consoleInputReader) {
+        System.out.println("Enter Employee ID: ");
+        int id = consoleInputReader.nextInt();
+        System.out.println("Enter Employee Name: ");
+        String name = consoleInputReader.next();
+        System.out.println("Enter Employee Salary: ");
+        double salary = consoleInputReader.nextDouble();
+        employeePayrollList.add(new EmployeePayrollData(id, name, salary));
+    }
+
+}
